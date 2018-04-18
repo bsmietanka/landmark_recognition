d2e3847f71814cf2cc4dd9950a2017eddf015e1f
RegyN <250901@pw.edu.pl>
2018-04-18 14:52:55 +0200

    Downloaded data is now split into training and validation sets.

--- download_dataset.py
+++ download_dataset.py
@@ -1,15 +1,22 @@
 import csv
 import argparse
-import os
+from os import listdir, rename, makedirs
+from os.path import isdir, join
 import urllib.request
+import random
 from multiprocessing.dummy import Pool as ThreadPool
 
-# TODO: change dataset directory structure
-#   dataset/
-#     |--> train/
-#     |    |--> {classes}/
-#     |--> test/
-#          |--> {classes}/
+
+def split_dataset(data_path, validation_path, threshold):
+    classes = [d for d in listdir(data_path) if isdir(join(data_path, d))]
+
+    makedirs(validation_path, exist_ok=True)
+    for cls_dir in classes:
+        for photo in listdir(data_path + cls_dir):
+            if random.uniform(0, 100) < threshold:
+                makedirs(validation_path + '/' + cls_dir, exist_ok=True)
+                rename(data_path + cls_dir + '/' + photo, validation_path + cls_dir + '/' + photo)
+
 
 class ImageDownloader:
     def __init__(self, path):
@@ -21,7 +28,7 @@ class ImageDownloader:
         if self.count % 100 == 0:
             print("Pobrano już " + str(self.count) + "/" + str(self.total)+" obrazków.")
         self.count = self.count+1
-        os.makedirs(self.directory + '/' + urlRow[2], exist_ok=True)
+        makedirs(self.directory + '/' + urlRow[2], exist_ok=True)
         try:
             urllib.request.urlretrieve(urlRow[1], self.directory + '/' + urlRow[2] + '/' + urlRow[0] + ".jpg")
         except:
@@ -36,9 +43,11 @@ class ImageDownloader:
 
 def main():
     parser = argparse.ArgumentParser(description='Download images specified in .csv file')
-    parser.add_argument('--inputFile', default='csv/dataset_filtered.csv', help='path to input csv data file')
-    parser.add_argument('--images', default="dataset", help='Where to save images')
-    parser.add_argument('--threads', default="4", help='How many threads to use')
+    parser.add_argument('--inputFile', default='dataset/trainFiltered.csv', help='path to input csv data file')
+    parser.add_argument('--images', default="dataset", help='Where to save training images')
+    parser.add_argument('--validation', default="validation", help='Where to save validation images')
+    parser.add_argument('--split', default="20", help='Percentage of images to be used as validation data')
+    parser.add_argument('--threads', default="24", help='How many threads to use')
     args = parser.parse_args()
 
     print("Rozpoczęto działanie skryptu")
@@ -50,6 +59,8 @@ def main():
 
     imDownloader = ImageDownloader(args.images)
     imDownloader.save_images_multithreaded(urlData, int(args.threads))
+    split_dataset(args.images+'/', args.validation+'/', int(args.split))
+
     print("Pobrano zdjęcia")
 
 
--- split_dataset.py
+++ split_dataset.py
@@ -1,22 +1,27 @@
-from os import listdir, rename
+from os import listdir, rename, makedirs
 from os.path import isdir, join
 import random
 
 # TODO: tidy up
 # TODO: merge with download_dataset.py
-# TODO: script works only if destination directories exist, must be fixed
+
+
+def split_dataset(data_path, validation_path, threshold):
+    classes = [d for d in listdir(data_path) if isdir(join(data_path, d))]
+
+    makedirs(validation_path, exist_ok=True)
+    for cls_dir in classes:
+        for photo in listdir(data_path + cls_dir):
+            if random.uniform(0, 100) < threshold:
+                makedirs(validation_path + '/' + cls_dir, exist_ok=True)
+                rename(data_path + cls_dir + '/' + photo, validation_path + cls_dir + '/' + photo)
+
 
 if __name__ == "__main__":
     path = "./dataset/"
     threshold = 20
-    validation_path = "./validation/"
+    val_path = "./validation/"
+    split_dataset(path, val_path, threshold)
 
-    classes = [d for d in listdir(path) if isdir(join(path, d))]
-    
-    for cls_dir in classes:
-        for photo in listdir(path + cls_dir):
-            if random.uniform(0,100) < threshold:
-                rename(join(path, cls_dir, photo), join(validation_path, cls_dir, photo))
-    
     print("Done.")
 
